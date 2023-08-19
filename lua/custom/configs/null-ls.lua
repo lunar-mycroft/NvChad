796fc3e2dbb66e797bc20f5980ac87e63173bddd
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2023-08-18 23:47:14 -0400

    stupid bugs

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,7 +1,8 @@
-local null_ls = require('null_ls')
+local null_ls = require('null-ls')
 
 local opts = {
   sources = {
+    null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff
   }
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -3,6 +3,7 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "black",
         "mypy",
         "pyright",
         "ruff"
